e961ebc3ae3ce1b9b8c395d2070bedc626ca2a9f
Abhijit Devghare <kishordevghare6@gmail.com>
2024-07-06 07:45:06 +0530

    Second Commit

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,5 @@
 import "./Projects.css"
 import triFest from "../../Images/picture1.png"
-import Project from "../Project/Project"
 import imageStation from "../../Images/imageStation.png"
 
 function Projects()
@@ -8,9 +7,20 @@ function Projects()
     return (
         <>
             <div id="projects">
-                <Project src={triFest} url={"https://college-registration-website.vercel.app/"} tag={"College Tri-Fest Registration system"}/> 
-                <Project src={imageStation} url={"https://image-station-photography.vercel.app/"} tag={"Photography website"}/>
- 
+
+                <div className="project">
+                <a href={"https://image-station-photography.vercel.app/"}>
+                    <h1>Photography website</h1>
+                    <img src={imageStation} alt="" /> <br />
+                </a>
+            </div>   
+            
+            <div className="project">
+                <a href={"https://college-registration-website.vercel.app/"}>
+                    <h1>Tri-Fest Registration system</h1>
+                    <img src={triFest} alt="" /> <br />
+                </a>
+            </div>
 
             </div> 
         </>
